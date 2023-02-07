2af00a1cc18b19bd3cd94cf5ce2d8023c1806b0d
Chucks Nwosu <chucksn611@gmail.com>
2023-02-07 10:53:03 -0600

    portfolio init

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="main w-screen min-h-screen bg-slate-600 ">
+        <h1 className="text-blue-800">hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
