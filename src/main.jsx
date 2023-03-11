5e2915580ca40471461601decc4373434d76f0e3
Chucks Nwosu <chucksn611@gmail.com>
2023-03-11 15:00:53 -0600

    vercel analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import "./index.css";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
