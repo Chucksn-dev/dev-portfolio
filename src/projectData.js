6d3617750efacbd00ed61cedb0fef316f5d99dfd
Chucks Nwosu <chucksn611@gmail.com>
2023-03-15 00:18:26 -0500

    project added

--- src/projectData.js
+++ src/projectData.js
@@ -4,6 +4,7 @@ import calculatorProjImg from "../src/assets/me/calculator-app.png";
 import movieAppProjImg from "../src/assets/me/movie-app.png";
 import radioAppProjImg from "../src/assets/me/radio-app.png";
 import weatherAppProjImg from "../src/assets/me/weather-app.png";
+import quizAppProjImg from "../src/assets/me/quiz-app.png";
 
 export const projectData = [
   {
@@ -21,18 +22,26 @@ export const projectData = [
       "Movie streaming app, developed with React and Redux. 100% functional",
   },
   {
-    name: "My Personal Portfolio",
-    url: "https://chucksn-portfolio.vercel.app/",
-    image: portfolioImg,
+    name: "Trivia Titan",
+    url: "https://trivia-titans.vercel.app/",
+    image: quizAppProjImg,
     description:
-      "Personal portfolio website. Includes projects and contact info",
+      "A dynamic quiz game app, that tests you on different categories and difficulty ",
   },
+
   {
     name: "Weather App",
     url: "http://weather-app-react-lyart.vercel.app/",
     image: weatherAppProjImg,
     description:
       "A real-time app that gives weather info by accessing your location or by search",
+  },
+  {
+    name: "My Personal Portfolio",
+    url: "https://chucksn-portfolio.vercel.app/",
+    image: portfolioImg,
+    description:
+      "Personal portfolio website. Includes projects and contact info",
   },
   {
     name: "Calculator App",
