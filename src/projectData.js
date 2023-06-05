b0103dbd5797988ded15610cbbd3e26c7332e8ec
Chucks Nwosu <chucksn611@gmail.com>
2023-06-05 11:54:08 -0500

    project data updated

--- src/projectData.js
+++ src/projectData.js
@@ -11,22 +11,25 @@ export const projectData = [
     name: "Mv-Hub",
     url: "https://movie-app-chi-nine.vercel.app/",
     image: movieAppProjImg,
+    features: "",
     tech: "JavaScript, React.js, Redux, Tailwind, Swiper-react, REST-API, Node.js, Express.js, MongoDB.",
     description:
       "A full-stack movie streaming web app that can access a huge library of movies from different genres. Users browse movies by categories or search for specific titles. Users can also create and delete personalized watchlists. Also has user login and sign-up features.",
   },
   {
     name: "World Radio",
-    url: "https://chucksn.github.io/World-Radio/",
+    url: "https://world-radio.vercel.app/",
     image: radioAppProjImg,
+    features: "",
     tech: "JavaScript, React.js, Redux-Toolkit, Tailwind, Framer-motion, REST-API.",
     description:
-      "A web-based application that allows users to listen to radio stations from around the world. Streams Over 20,000 Internet Radio Stations in real time. Easy to use and has 'Search Country' and 'Add to favorite' features.",
+      "A Full-stack web-based application that allows users to listen to radio stations from around the world. Streams Over 20,000 Internet Radio Stations in real time. Easy to use and has 'Search Country' and 'Add to favorite' features.",
   },
   {
     name: "Trivia Titan",
     url: "https://trivia-titans.vercel.app/",
     image: quizAppProjImg,
+    features: "",
     tech: "JavaScript, React.js, Redux-Toolkit, Tailwind, Framer-motion, REST-API.",
     description:
       "A dynamic quiz web app that offers users the opportunity to test their knowledge on a variety of subjects. The app typically features a range of quizzes, each with a set of questions and multiple choice answers.",
@@ -36,6 +39,7 @@ export const projectData = [
     name: "Weather App",
     url: "http://weather-app-react-lyart.vercel.app/",
     image: weatherAppProjImg,
+    features: "",
     tech: "JavaScript, React.js, Redux, Context-API, REST-API, Geo-location API, swiper.js.",
     description:
       "A web application that provides users with real-time weather information based on their current location. The app uses a geo-location API to automatically detect the user's location and provide weather updates for that specific area.",
@@ -44,6 +48,7 @@ export const projectData = [
     name: "Personal Portfolio",
     url: "https://chucksn-portfolio.vercel.app/",
     image: portfolioImg,
+    features: "",
     tech: "JavaScript, React.js, Redux-Toolkit, Tailwind, Framer-motion, Intersection-observer API ",
     description:
       "Personal portfolio website. Includes, info about me, my technical abilities, projects and contact info",
@@ -52,6 +57,7 @@ export const projectData = [
     name: "Calculator App",
     url: "https://calculator-app-chucksn.vercel.app/",
     image: calculatorProjImg,
+    features: "",
     tech: "HTML, Vanilla CSS, JavaScript",
     description:
       "A Calculator web application that functions just like a real calculator.",
@@ -60,6 +66,7 @@ export const projectData = [
     name: "Analog Clock Web App",
     url: "https://analog-clock-chucksn.vercel.app/",
     image: analogClockProjImg,
+    features: "",
     tech: "HTML, Vanilla CSS, JavaScript",
     description: "Analog clock web application with manual setting adjustment.",
   },
